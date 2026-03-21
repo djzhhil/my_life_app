7a51234dd21d3fdbf87a823a5c7c0d813f5b6d8d
djzhhil <zguangqi52@gmail.com>
2026-03-21 11:39:06 +0800

    feat: 添加CorsConfig，修改开发环境跨端验证

--- src/main/java/store/scserver/my_life_app/config/SecurityConfig.java
+++ src/main/java/store/scserver/my_life_app/config/SecurityConfig.java
@@ -14,7 +14,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
+        http.cors(cors -> {})
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
         return http.build();
